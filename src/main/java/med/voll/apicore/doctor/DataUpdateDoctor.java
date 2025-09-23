c95d291e3b13645b297c725698f3efb84a19f3c8
Mikael Maia <mika.sjj@hotmail.com>
2025-09-23 08:33:20 -0300

    feat: DTO that guarantees the correct fields when updating data

--- src/main/java/med/voll/apicore/doctor/DataUpdateDoctor.java
+++ src/main/java/med/voll/apicore/doctor/DataUpdateDoctor.java
@@ -1,4 +1,14 @@
 package med.voll.apicore.doctor;
 
-public record DataUpdateDoctor() {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import med.voll.apicore.address.DataAddress;
+
+public record DataUpdateDoctor(
+        @NotNull
+        Long id,
+        String name,
+        String phone,
+        @Valid
+        DataAddress address) {
 }
