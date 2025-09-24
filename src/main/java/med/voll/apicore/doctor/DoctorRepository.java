6fa8f98a4d32965d874f1c0f898b87a84efa1c2e
Mikael Maia <mika.sjj@hotmail.com>
2025-09-24 09:02:39 -0300

    feat: Doctor repository inteface implementation

--- src/main/java/med/voll/apicore/doctor/DoctorRepository.java
+++ src/main/java/med/voll/apicore/doctor/DoctorRepository.java
@@ -1,6 +1,9 @@
 package med.voll.apicore.doctor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Page<Doctor> findAllByActiveTrue(Pageable pagination);
 }
