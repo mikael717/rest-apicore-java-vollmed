1f244d67faf8bf94d6954a7a7cb01b5b7f535ee1
Mikael Maia <mika.sjj@hotmail.com>
2025-09-25 11:49:37 -0300

    refactor: Controller return pattern

--- src/main/java/med/voll/apicore/controller/DoctorController.java
+++ src/main/java/med/voll/apicore/controller/DoctorController.java
@@ -1,22 +1,27 @@
 package med.voll.apicore.controller;
 
 import jakarta.validation.Valid;
+import med.voll.apicore.doctor.DataDetailsDoctor;
 import med.voll.apicore.doctor.DataListingDoctor;
 import med.voll.apicore.doctor.DataRegisterDoctor;
+import med.voll.apicore.doctor.DataUpdateDoctor;
 import med.voll.apicore.doctor.Doctor;
 import med.voll.apicore.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("doctors")
@@ -27,12 +32,42 @@ public class DoctorController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody @Valid DataRegisterDoctor data){
-        repository.save(new Doctor(data));
+    public ResponseEntity register(@RequestBody @Valid DataRegisterDoctor data, UriComponentsBuilder uriBuilder){
+        var doctor = new Doctor(data);
+        repository.save(doctor);
+        var uri = uriBuilder.path("/doctors/{id}").buildAndExpand(doctor.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DataDetailsDoctor(doctor));
     }
 
     @GetMapping
-    public Page<DataListingDoctor> listing(@PageableDefault(sort = {"name"}, size = 10) Pageable pagination){
-        return repository.findAll(pagination).map(DataListingDoctor::new);
+    public ResponseEntity <Page<DataListingDoctor>> listing(@PageableDefault(sort = {"name"}, size = 10) Pageable pagination){
+        var page = repository.findAllByActiveTrue(pagination).map(DataListingDoctor::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity update (@RequestBody @Valid DataUpdateDoctor data){
+        var doctor = repository.getReferenceById(data.id());
+        doctor.updateInformation(data);
+
+        return ResponseEntity.ok(new DataDetailsDoctor(doctor));
+    }
+
+    @DeleteMapping("{id}")
+    @Transactional
+    public ResponseEntity delete (@PathVariable Long id){
+        var doctor = repository.getReferenceById(id);
+        doctor.logicalDelete();
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity detail(@PathVariable Long id){
+        var doctor = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DataDetailsDoctor(doctor));
     }
 }
