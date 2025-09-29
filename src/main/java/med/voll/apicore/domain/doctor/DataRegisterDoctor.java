9e1f3bdd62ecf7528709dece45057bd606682ff8
Mikael Maia <mika.sjj@hotmail.com>
2025-09-29 11:07:49 -0300

    feat: implement new attributes dto register

--- src/main/java/med/voll/apicore/domain/doctor/DataRegisterDoctor.java
+++ src/main/java/med/voll/apicore/domain/doctor/DataRegisterDoctor.java
@@ -1,26 +1,26 @@
-package med.voll.apicore.doctor;
+package med.voll.apicore.domain.doctor;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.apicore.address.DataAddress;
+import med.voll.apicore.domain.address.DataAddress;
 
 public record DataRegisterDoctor(
-        @NotBlank
+        @NotBlank(message = "{mandatory.name}")
         String name,
-        @NotBlank
-        @Email
+        @NotBlank(message = "{mandatory.email}")
+        @Email(message = "{invalid.name}")
         String email,
-        @NotBlank
+        @NotBlank(message = "{mandatory.phone}")
         String phone,
-        @NotBlank
-        @Pattern(regexp = "\\d{4,6}")
+        @NotBlank(message = "{mandatory.crm}")
+        @Pattern(regexp = "\\d{4,6}", message = "{invalid.crm}")
         String crm,
-        @NotNull
+        @NotNull(message = "{mandatory.specialty}")
         Specialty specialty,
-        @NotNull
+        @NotNull(message = "{mandatory.address}")
         @Valid
         DataAddress address) {
 }
