cacfd02b1dc2ec01acae872c83cdab4642288bff
Mikael Maia <mika.sjj@hotmail.com>
2025-09-30 11:13:27 -0300

    feat: new migration for user entity

--- src/main/resources/db/migration/V6__create-table-users.sql
+++ src/main/resources/db/migration/V6__create-table-users.sql
@@ -1,16 +1,8 @@
-create table patients(
+create table users(
+
     id bigint not null auto_increment,
-    name varchar(100) not null,
-    email varchar(100) not null unique,
-    cpf varchar(14) not null unique,
-    phone varchar(20) not null,
-    street varchar(100) not null,
-    neighborhood varchar(100) not null,
-    zipcode varchar(9) not null,
-    complement varchar(100),
-    number varchar(100),
-    state char(2) not null,
-    city varchar(100) not null,
+    login varchar(100) not null,
+    password varchar(255) not null,
 
     primary key(id)
 );
