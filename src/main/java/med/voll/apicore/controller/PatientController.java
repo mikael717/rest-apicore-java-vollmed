7d67005cf9ff573f9d879ec903de66df95109730
Mikael Maia <mika.sjj@hotmail.com>
2025-10-03 11:03:37 -0300

    refactor: change in method return

--- src/main/java/med/voll/apicore/controller/PatientController.java
+++ src/main/java/med/voll/apicore/controller/PatientController.java
@@ -1,20 +1,27 @@
 package med.voll.apicore.controller;
 
 import jakarta.validation.Valid;
-import med.voll.apicore.patient.DataListingPatient;
-import med.voll.apicore.patient.DataRegisterPatient;
-import med.voll.apicore.patient.Patient;
-import med.voll.apicore.patient.PatientRepository;
+import med.voll.apicore.domain.patient.DataDetailsPatient;
+import med.voll.apicore.domain.patient.DataListingPatient;
+import med.voll.apicore.domain.patient.DataRegisterPatient;
+import med.voll.apicore.domain.patient.DataUpdatePatient;
+import med.voll.apicore.domain.patient.Patient;
+import med.voll.apicore.domain.patient.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("patients")
@@ -25,12 +32,42 @@ public class PatientController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody @Valid DataRegisterPatient data){
-        repository.save(new Patient(data));
+    public ResponseEntity register(@RequestBody @Valid DataRegisterPatient data, UriComponentsBuilder uriBuilder){
+        var patient = new Patient(data);
+        repository.save(patient);
+        var uri = uriBuilder.path("/patients/{id}").buildAndExpand(patient.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DataDetailsPatient(patient));
     }
 
     @GetMapping
-    public Page<DataListingPatient> listing(@PageableDefault(sort = {"name"}, size = 10) Pageable pagination){
-        return repository.findAll(pagination).map(DataListingPatient::new);
+    public ResponseEntity<Page<DataListingPatient>> listing(@PageableDefault(sort = {"name"}, size = 10) Pageable pagination){
+        var page = repository.findAllByActiveTrue(pagination).map(DataListingPatient::new);
+        return ResponseEntity.ok(page);
+
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity update(@RequestBody @Valid DataUpdatePatient data){
+        var patient = repository.getReferenceById(data.id());
+        patient.updateInformation(data);
+
+        return ResponseEntity.ok(new DataDetailsPatient(patient));
+    }
+
+    @DeleteMapping("{id}")
+    @Transactional
+    public ResponseEntity delete(@PathVariable Long id){
+        var patient = repository.getReferenceById(id);
+        patient.logicalDelete();
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity detail (@PathVariable Long id){
+        var patient = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new DataDetailsPatient(patient));
     }
 }
