8db9c02a8725d4ae51d4901fbb81170ccb5444b7
Mikael Maia <mika.sjj@hotmail.com>
2025-10-06 09:41:50 -0300

    refactor: repository new architectural structure

--- src/main/java/med/voll/apicore/domain/patient/PatientRepository.java
+++ src/main/java/med/voll/apicore/domain/patient/PatientRepository.java
@@ -1,10 +1,8 @@
-package med.voll.apicore.patient;
+package med.voll.apicore.domain.patient;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     Page<Patient> findAllByActiveTrue(Pageable pagination);
