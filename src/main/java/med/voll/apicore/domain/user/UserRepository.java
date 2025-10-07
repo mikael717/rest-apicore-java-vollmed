a541e0c4b9b797973961734a116b8703d42da37c
Mikael Maia <mika.sjj@hotmail.com>
2025-10-07 10:55:47 -0300

    feat: new repository user

--- src/main/java/med/voll/apicore/domain/user/UserRepository.java
+++ src/main/java/med/voll/apicore/domain/user/UserRepository.java
@@ -1,4 +1,8 @@
 package med.voll.apicore.domain.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
 }
