ce1289c073255251504177e15da009d992268f30
Mikael Maia <mika.sjj@hotmail.com>
2025-10-16 09:22:41 -0300

    feat: implements authentication service in the api

--- src/main/java/med/voll/apicore/domain/user/AuthenticationService.java
+++ src/main/java/med/voll/apicore/domain/user/AuthenticationService.java
@@ -1,4 +1,19 @@
 package med.voll.apicore.domain.user;
 
-public class AuthenticationService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
 }
