dea0481a6d2c75b224d813e4d4ede4b4f5d6bd97
Mikael Maia <mika.sjj@hotmail.com>
2025-10-16 09:21:08 -0300

    feat: implement security validation

--- src/main/java/med/voll/apicore/controller/PatientController.java
+++ src/main/java/med/voll/apicore/controller/PatientController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -58,6 +59,7 @@ public class PatientController {
 
     @DeleteMapping("{id}")
     @Transactional
+    @Secured("ROLE_ADMIN")
     public ResponseEntity delete(@PathVariable Long id){
         var patient = repository.getReferenceById(id);
         patient.logicalDelete();
