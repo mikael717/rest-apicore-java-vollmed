7e2c30bdfdd6be7d88fc89cae80f3800349c3acf
Mikael Maia <mika.sjj@hotmail.com>
2025-10-23 09:24:34 -0300

    feat: handles exceptions specifically

--- src/main/java/med/voll/apicore/infra/exception/ErrorHandler.java
+++ src/main/java/med/voll/apicore/infra/exception/ErrorHandler.java
@@ -1,11 +1,18 @@
-package med.voll.apicore.infra;
+package med.voll.apicore.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.AuthenticationException;
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -19,6 +26,30 @@ public class ErrorHandler {
     public ResponseEntity handleError400(MethodArgumentNotValidException ex){
         var errors = ex.getFieldErrors();
         return ResponseEntity.badRequest().body(errors.stream().map(DataValidationError::new).toList());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handleErorr400(HttpMessageNotReadableException ex){
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity handleErrorBadCredentials(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity handleErrorAuthentication(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failure");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity handleErrorAccessDenied(){
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied");
+    }
+
+    public ResponseEntity handleError500(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + ex.getLocalizedMessage());
     }
 
     private record DataValidationError(String field, String message){
