4273718ce2fa4b1831878430b45efc9a319ef27e
Mikael Maia <mika.sjj@hotmail.com>
2025-10-29 10:38:41 -0300

    feat: implements new return treatment

--- src/main/java/med/voll/apicore/controller/ConsultationController.java
+++ src/main/java/med/voll/apicore/controller/ConsultationController.java
@@ -3,6 +3,8 @@ package med.voll.apicore.controller;
 import jakarta.validation.Valid;
 import med.voll.apicore.domain.consultation.DataDetailsConsultation;
 import med.voll.apicore.domain.consultation.DataScheduleConsultation;
+import med.voll.apicore.domain.consultation.ScheduleConsultation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/consultations")
 public class ConsultationController {
 
+    @Autowired
+    private ScheduleConsultation scheduleConsultation;
+
     @PostMapping
     @Transactional
-    public ResponseEntity schedule(@RequestBody @Valid DataScheduleConsultation data){
-        System.out.println(data);
+    public ResponseEntity schedule(@RequestBody @Valid DataScheduleConsultation date){
+        scheduleConsultation.scheduler(date);
         return ResponseEntity.ok(new DataDetailsConsultation(null, null, null, null));
     }
 }
