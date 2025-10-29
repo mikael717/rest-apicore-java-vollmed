9d556b9f006f139eca902e12e60fc04d92838caa
Mikael Maia <mika.sjj@hotmail.com>
2025-10-29 10:39:49 -0300

    feat: implements entity consultation

---
+++ src/main/java/med/voll/apicore/domain/consultation/Consultation.java
@@ -0,0 +1,62 @@
+package med.voll.apicore.domain.consultation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import med.voll.apicore.domain.doctor.Doctor;
+import med.voll.apicore.domain.patient.Patient;
+
+import java.time.LocalDateTime;
+
+@Table(name = "consultations")
+@Entity(name = "Consultation")
+@EqualsAndHashCode(of = "id")
+public class Consultation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    public Consultation(){}
+
+    public Consultation(Long id, Doctor doctor, Patient patient, LocalDateTime dateTime) {
+        this.id = id;
+        this.doctor = doctor;
+        this.patient = patient;
+        this.dateTime = dateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+}
