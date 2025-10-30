e06149b4ec54bfd8195681e73231f8d558f3abf5
Mikael Maia <mika.sjj@hotmail.com>
2025-10-30 10:19:40 -0300

    feat: implements new attributes

--- src/main/java/med/voll/apicore/domain/consultation/Consultation.java
+++ src/main/java/med/voll/apicore/domain/consultation/Consultation.java
@@ -2,6 +2,8 @@ package med.voll.apicore.domain.consultation;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import med.voll.apicore.domain.doctor.Doctor;
 import med.voll.apicore.domain.patient.Patient;
@@ -35,13 +38,18 @@ public class Consultation {
     @Column(name = "date_time")
     private LocalDateTime dateTime;
 
+    @Column(name = "cancellation_reason")
+    @Enumerated(EnumType.STRING)
+    private CancellationReason cancellationReason;
+
     public Consultation(){}
 
-    public Consultation(Long id, Doctor doctor, Patient patient, LocalDateTime dateTime) {
+    public Consultation(Long id, Doctor doctor, Patient patient, LocalDateTime dateTime, CancellationReason cancellationReason) {
         this.id = id;
         this.doctor = doctor;
         this.patient = patient;
         this.dateTime = dateTime;
+        this.cancellationReason = cancellationReason;
     }
 
     public Long getId() {
@@ -58,5 +66,9 @@ public class Consultation {
 
     public LocalDateTime getDateTime() {
         return dateTime;
+    }
+
+    public void cancel(@NotNull CancellationReason reason) {
+        this.cancellationReason = reason;
     }
 }
