d7dc5945afe0e356f2bb0358520645cc9503546c
Mikael Maia <mika.sjj@hotmail.com>
2025-11-10 11:23:38 -0300

    feat: respository for consultations

--- src/main/java/med/voll/apicore/domain/consultation/ConsultationRepository.java
+++ src/main/java/med/voll/apicore/domain/consultation/ConsultationRepository.java
@@ -1,6 +1,14 @@
 package med.voll.apicore.domain.consultation;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
+
+    boolean existsByDoctorIdAndDateTime(Long idDoctor, @NotNull @Future LocalDateTime dateTime);
+
+    boolean existsByPatientIdAndDateTimeBetween(Long idPatient, LocalDateTime firstTime, LocalDateTime lastTime);
 }
